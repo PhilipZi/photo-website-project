e3306d768a1aa7d32a0b637e6d16e0012c07e81f
Philip Zimmermann <philip.zimmermann@freenet.de>
2022-09-13 11:43:43 +0200

    delete test folder utils

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,12 +1,5 @@
 import StyledButton from './styled';
 
-/**
- *
- * @param {React.ReactNode} children
- * @param {React.HTMLAttributes<HTMLButtonElement>} props
- * @return {JSX.Element}
- * @constructor
- */
 export default function Button({children, type = 'button', ...props}) {
 	return (
 		<StyledButton type={type} {...props}>
@@ -14,5 +7,3 @@ export default function Button({children, type = 'button', ...props}) {
 		</StyledButton>
 	);
 }
-
-Button.propTypes = {};
--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -1,11 +1,8 @@
 import styled from 'styled-components';
 
-import {pxToRem} from '../../utils/unit';
-
 const StyledButton = styled.button`
 	padding: 0.5em 1em;
 	border: 1px solid rgba(0, 0, 0, 0.3);
-	border-radius: ${pxToRem(4)};
 	background: #fae;
 	color: #000;
 	font-size: 1em;
