f3852354ed5f734a952edea76d4a3f2bc317324f
Philip Zimmermann <philip.zimmermann@freenet.de>
2022-09-13 12:32:34 +0200

    add bootstrap delete template files

--- src/components/Button/index.js
+++
@@ -1,9 +0,0 @@
-import StyledButton from './styled';
-
-export default function Button({children, type = 'button', ...props}) {
-	return (
-		<StyledButton type={type} {...props}>
-			{children}
-		</StyledButton>
-	);
-}
